b3c90495591f31a3602d54e139ac1c7461663249
redspirit <redspirit2@ya.ru>
2013-09-05 21:22:38 +0800

    added imageCrop

--- jquery.squarecrop.js
+++ jquery.squarecrop.js
@@ -1,3 +1,14 @@
+/**
+ * jQuery.squareCrop - crop and resize images on browser
+ * Written by Alexey Tayanchin (RedSpirit)
+ * Website: http://redspirit.ru
+ * EMail: redspirit@live.ru
+ * Date: 2013/09/05
+ *
+ * @author Alexey Tayanchin
+ * @version 1.0
+ *
+ **/
 (function($) {
 $.fn.squareCrop = function(options) {
 	var settings = $.extend( {
@@ -42,4 +53,62 @@ $.fn.squareCrop = function(options) {
 
 	});
 };
+
+$.fn.imageCrop = function(options) {
+	var settings = $.extend( {
+		'left' : 0,
+		'top' : 0,
+		'right' : 0,
+		'bottom' : 0,
+		'width' : false,
+		'height' : false,
+		'wrapClass': 'imagecrop'
+    }, options);
+	
+	function isImageLoaded(img) {
+		if (!img.complete) return false;
+		if (typeof img.naturalWidth !== "undefined" && img.naturalWidth === 0) return false;
+		return true;
+	}	
+	
+	this.each(function() {
+		var img = $(this);
+		var block = document.createElement('div');
+		
+		
+		if(img.is('img')){
+			img.on('load', function(){
+
+				var img_w = img.width();
+				var img_h = img.height();
+				var asp_w = this.naturalWidth / img_w;
+				var asp_h = this.naturalHeight / img_h;
+				
+				// do not work in IE < 9
+				// todo use http://www.jacklmoore.com/notes/naturalwidth-and-naturalheight-in-ie/
+				var cont_w = img_w - Math.round((settings.left + settings.right) / asp_w);
+				var cont_h = img_h - Math.round((settings.top + settings.bottom) / asp_h);
+													
+				$(block).css({
+					width: cont_w + 'px',
+					height: cont_h + 'px',
+					overflow: 'hidden',
+					display: 'inline-block'
+				}).addClass(settings.wrapClass);
+				img.wrap(block);
+			
+				img.css({
+					'marginLeft': -Math.round(settings.left / asp_w),
+					'marginTop': -Math.round(settings.top / asp_h)
+				});
+
+			});
+			if(isImageLoaded(this)) {
+				img.trigger('load');
+			}
+		}
+
+	});
+};
+
 })(jQuery);
